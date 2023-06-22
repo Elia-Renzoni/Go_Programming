39981e441e3880df3eb7e6bb1353421765647292
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-06-22 17:21:02 +0200

    Add files via upload

---
+++ Chapter-1/sum.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+const primo_numero int = 10
+const secondo_numero int = 20
+
+func main() {
+	var somma int
+	somma = primo_numero + secondo_numero
+	fmt.Println(somma)
+}
