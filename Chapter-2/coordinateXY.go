f0eb104802d2ebf3759e6e584d824ce317785281
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-07-19 14:05:49 +0200

    Add files via upload

--- Chapter-2/coordinateXY.go
+++ Chapter-2/coordinateXY.go
@@ -1,7 +1,3 @@
-
-// TODO
-
-
 package main 
 
 import "fmt"
@@ -11,8 +7,34 @@ const (
 	secondoQuadrante = 2
 	terzoQuadrante = 3
 	quartoQuadrante = 4
+	controlloQuadrante float32 = 0.0
 )
 
 func main() {
+	var xCoord, yCoord float32 = -1.0, -2.5
+	if quadrante := controllaGrafico(xCoord, yCoord); quadrante == primoQuadrante {
+		fmt.Println("le Coordinate di trovano nel primo quadrante")
+	} else if quadrante == secondoQuadrante {
+		fmt.Println("le Coordinate si trovano nel secondo quadrante")
+	} else if quadrante == terzoQuadrante {
+		fmt.Println("le Coordinate si trovano nel terzo quadrante")
+	} else {
+		fmt.Println("le Coordinate si trovano nel quarto quadrante")
+	}
 
 }
+
+func controllaGrafico(x float32, y float32) int8 {
+	var quadrante int8 
+	if x < controlloQuadrante && y < controlloQuadrante {
+		quadrante = terzoQuadrante
+	} else if x > controlloQuadrante && y < controlloQuadrante {
+		quadrante = quartoQuadrante
+	} else if x > controlloQuadrante && y > controlloQuadrante {
+		quadrante = primoQuadrante
+	} else if x < controlloQuadrante && y > controlloQuadrante {
+		quadrante = secondoQuadrante
+	}
+	return quadrante
+}
+
