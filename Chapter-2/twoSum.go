33df25181afcd375ae4feae5760257c9314fc85a
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-07-21 18:10:56 +0200

    Add files via upload

--- Chapter-2/twoSum.go
+++ Chapter-2/twoSum.go
@@ -5,15 +5,19 @@ import "fmt"
 const notFound = -1
 
 func main() {
-	var arrElements [5]int16 = [5]int16{12, 44, 3, 6, 8, 7}
+	var arrElements [6]int16 = [6]int16{12, 44, 3, 6, 8, 7}
 	var target int16 = 34
-	var firstIndex, secondIndex int16 = controlSum(arrElements, target)
+	var firstIndex, secondIndex int = controlSum(arrElements, target)
 	var result bool = setOutput(firstIndex, secondIndex) 
-
+	if result {
+		fmt.Printf("Trovato !")
+	} else {
+		fmt.Printf("Non trovato !")
+	}
 }
 
-func controlSum(array []int16, target int16) int16 {
-	var firstIndex, secondIndex int16
+func controlSum(array [6]int16, target int16) int {
+	var firstIndex, secondIndex int
 	for index := range array {
 		if sum := array[index] + array[index + 1]; sum == target {
 			firstIndex, secondIndex = index, index + 1
@@ -24,7 +28,7 @@ func controlSum(array []int16, target int16) int16 {
 	return firstIndex, secondIndex
 }
 
-func setOutput(index1 int16, index2 int16) bool {
+func setOutput(index1 int, index2 int) bool {
 	if index1 != notFound && index2 != notFound {
 		return false
 	} else {
