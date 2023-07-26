b5f5bfba35b59b2cb1a68f4fac6feb57a631608b
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-07-26 08:56:16 +0200

    Add files via upload

---
+++ Chapter-3/powerSet.go
@@ -0,0 +1,38 @@
+/*
+* @author Elia Renzoni
+* @date 26/07/2023
+* @brief return the power set of an array (integer)
+*/
+
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+  var (
+    inputSet []int = []int {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    powerSetSubSet int
+    powerSet []int = setPowerSet(&powerSetSubSet, inputSet)
+  )
+  if powerSetSubSet != 0 {
+    for _, value := range powerSet {
+      fmt.Printf("Value Elem : %d", value)
+    }
+  } else {
+    fmt.Printf("No PowerSet")
+  }
+}
+
+func setPowerSet(subSetNumbers* int, inputSet []int) []int {
+  var powerSet []int
+  if *subSetNumbers = math.Pow(2, len(inputSet)); *subSetNumbers != 0 {
+      subSetCounter := 1
+      for i, j := 0; j < subSetCounter && subSetCounter <= *subSetNumbers && j < len(inputSet) - 1; j++, subSetCounter += 1, i += 1 {
+           powerSet[i] = inputSet[j]
+      }
+  } else {
+    return powerSet
+  }
+  return powerSet
+}
