53c7c0dd7b54c5b11f6a42a7ebaaf7ce785429e0
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-08-23 15:56:04 +0200

    Add files via upload

---
+++ Chapter-4/ksumPairs.go
@@ -0,0 +1,48 @@
+/**
+*	@author Elia Renzoni
+*	@date 23/08/2023
+*	@brief go functions problems
+*
+**/
+
+package main
+
+import "fmt"
+import "math/rand"
+
+const (
+	maxSize = 12
+	minOp = 0
+)
+
+func main() {
+	var (
+		createArray = func() int[] {
+			var newArray[maxSize] int
+			for index := range newArray {
+				newArray[index] = rand.Intn(20)
+			}
+		}
+		kValue = func() int {
+			return rand.Intn(20)
+		} ()
+		numsArr = createArray()
+	)
+	if maxOp := maxOperation(numsArr, kValue); maxOp != minOp {
+		fmt.Printf("Operations : %d", maxOp)
+	} else {
+		fmt.Printf("No operations")
+	}
+}
+
+func maxOperation(nums []int, k int) (operations int) {
+	var operations int
+	for _, value := range nums {
+		for _, sValue := range nums {
+			if value + sValue == k {
+				operations++
+			}
+		}
+	}
+	return 
+}
