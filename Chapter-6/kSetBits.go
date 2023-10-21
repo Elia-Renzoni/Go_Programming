38bcb70761aee07ebf82e80a3f7f910858953704
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-10-21 08:54:24 +0200

    Add files via upload

--- Chapter-6/kSetBits.go
+++ Chapter-6/kSetBits.go
@@ -4,35 +4,41 @@
 *	@brief Slice
 **/
 
-package main 
+package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
-	"time"
 	"os"
+	"time"
 )
 
-const sigBit byte = 1
+const (
+	minuSigBit = iota
+	sigBit
+	div
+)
 
 func main() {
 	var (
-		err error
-		kValue int
+		err         error
+		kValue      int
 		sliceLenght int
-		sum int
+		sum         int
 	)
 
 	if err, kValue, sliceLenght = setValues(); err != nil {
 		fmt.Printf("Errore nell'acquisizione dei valori !")
 		os.Exit(1)
 	} else {
+		// TODO
 		if nums := make([]int, sliceLenght); nums == nil {
-			os.Exit(1);
+			os.Exit(1)
 		} else {
 			initNums(nums)
 			nBits(nums, &sum, kValue)
-			fmt.Printf("Somma : %d" sum)
+			fmt.Printf("Somma : %d", sum)
 		}
 	}
 }
@@ -45,39 +51,44 @@ func setValues() (err error, k int, lenght int) {
 		fmt.Printf("Inserici la lunghezza massima dello slice : \n")
 		_, err = fmt.Scanf("%d", &lenght)
 		if err != nil {
-			return errors.New("Errore nell'acquisizione !"), nil, nil
-		} 
-		if k < 0 || lenght < 0{
+			return errors.New("Errore nell'acquisizione !"), 0, 0
+		}
+		if k < 0 || lenght < 0 {
 			fmt.Printf("Errore, valodi non validi ! \n")
 		} else {
-			control = false;
-		} 
+			control = false
+		}
 	}
-	return 
+	return
 }
 
-func initNums(nums ...int) {
+func initNums(nums []int) {
 	rand.Seed(time.Now().Unix())
 	for index := range nums {
 		nums[index] = rand.Intn(100)
 	}
 }
 
-func nBits(nums ...int, sum* int, k int) {
+func nBits(nums []int, sum *int, k int) {
 	for _, value := range nums {
-		if result := encoding(value, k); result {
+		if result := encoding(&value, k); result {
 			*sum += value
 		}
-	} 
+	}
 }
 
-func encoding(value, k int) (result bool) {
-	// TODO
+func encoding(value *int, k int) (result bool) {
+	inc := 0
+	for *value >= 0 {
+		if *value%div == sigBit {
+			inc++
+		}
+		*value /= div
+	}
+	if inc == k {
+		result = true
+	} else {
+		result = false
+	}
+	return
 }
-
-
-
-
-
-
-
